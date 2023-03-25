6d38effad1a3fd16195d0f44fbeb7047ea714e87
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-03-25 18:23:26 +0100

    add Files from  project:  goit-markup-hw-01

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    overrides: [
+        {
+            files: ['*.html'],
+            parser: '@html-eslint/parser',
+            extends: ['plugin:@html-eslint/recommended'],
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        selfClosing: 'always',
+    },
+    plugins: ['react', '@typescript-eslint', '@html-eslint'],
+    rules: {
+        '@html-eslint/require-closing-tags': [
+            'error',
+            {
+                selfClosing: 'always',
+            },
+        ],
+        '@html-eslint/no-extra-spacing-attrs': [
+            'error',
+            {
+                enforceBeforeSelfClose: true,
+            },
+        ],
+        '@html-eslint/indent': ['error', 4],
+    },
+};
